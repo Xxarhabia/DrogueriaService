1dc6ac45b775516fe5a40c6984b723588941787f
MarioSarabia <mariosarabia@wposs.com>
2024-09-20 17:14:22 -0500

    📋 Se ajusta sp de anulacion de transacciones

--- queries/procedures/sp_annulment_transaction.sql
+++ queries/procedures/sp_annulment_transaction.sql
@@ -1,46 +1,50 @@
+-- PROCEDURE: drogueria_schema.sp_annulment_transaction(character varying)
+
+-- DROP PROCEDURE IF EXISTS drogueria_schema.sp_annulment_transaction(character varying);
+
 CREATE OR REPLACE PROCEDURE drogueria_schema.sp_annulment_transaction(
-	IN in_transaction_reference VARCHAR,
-	OUT out_message VARCHAR,
-	OUT out_status BOOLEAN,
-	OUT out_transaction_id BIGINT)
+	IN in_transaction_reference character varying,
+	OUT out_message character varying,
+	OUT out_status boolean,
+	OUT out_transaction_id bigint)
 LANGUAGE 'plpgsql'
-AS $$
+AS $BODY$
 DECLARE
-
+	
 	v_transaction_id BIGINT;
-
+	
 	v_ti_product_id BIGINT;
 	v_ti_quantity BIGINT;
-
+	
 	v_p_quantity INT;
-
+	
 	v_return_quantity_in_product INT;
 
 BEGIN
 	SELECT id INTO v_transaction_id
 	FROM drogueria_schema.transactions
 	WHERE reference = in_transaction_reference;
-
+	
 	IF v_transaction_id IS NOT NULL
 	THEN
 		UPDATE drogueria_schema.transactions
 		SET date_update_trx = TO_CHAR(NOW(), 'DD-MM-YYYY HH24:MI:SS'), type_trx = 'ANNULMENT'
 		WHERE id = v_transaction_id;
-
+		
 		SELECT id, quantity INTO v_ti_product_id, v_ti_quantity
-		FROM drogueria_schema.tansaction_item
+		FROM drogueria_schema.transaction_item
 		WHERE transaction_id = v_transaction_id;
-
-		SELECT quatity INTO v_p_quantity
+		
+		SELECT stock INTO v_p_quantity
 		FROM drogueria_schema.products
 		WHERE id = v_ti_product_id;
-
+		
 		v_return_quantity_in_product := v_p_quantity + v_ti_quantity;
-
+		
 		UPDATE drogueria_schema.products
-		SET quantity = v_return_quantity_in_product
+		SET stock = v_return_quantity_in_product
 		WHERE id = v_ti_product_id;
-
+		
 		out_message := 'Transaction successfully canceled';
 		out_status := TRUE;
 		out_transaction_id := v_transaction_id;
@@ -50,4 +54,6 @@ BEGIN
 		out_transaction_id := NULL;
 	END IF;
 END;
-$$;
+$BODY$;
+ALTER PROCEDURE drogueria_schema.sp_annulment_transaction(character varying)
+    OWNER TO postgres;
